e49b4555ba91220dd7482e652da422b82ccb87b6
chamika94 <chamikasandaruwan1994@gmail.com>
2022-05-01 18:55:44 +0530

    Implemented the all functions of signup part completly

--- frontend/src/redux/features/authSlice.js
+++ frontend/src/redux/features/authSlice.js
@@ -14,6 +14,20 @@ export const login = createAsyncThunk("auth/login",async({formValue, navigate, t
     }
 })
 
+export const register = createAsyncThunk("auth/register",async({formValue, navigate, toast},{rejectWithValue})=>{
+    try{
+      const response = await api.signUp(formValue);
+      toast.success("Registered Successfully");
+      navigate("/");
+      return response.data;
+     //console.log("resp",response);
+    }catch(err){
+        //console.log("resp",err.response.data.message);
+        return rejectWithValue(err.response.data);
+    }
+})
+
+
 const authSlice = createSlice({
     name:"Auth",
     initialState:{
@@ -34,6 +48,20 @@ const authSlice = createSlice({
             state.loading = false;
             state.error = action.payload.message;
         },
+//==================================================================================
+        [register.pending]:(state, action) => {
+            state.loading=true;
+        },
+        [register.fulfilled]:(state, action) => {
+            state.loading = false;
+            localStorage.setItem("profile",JSON.stringify({...action.payload}));
+            state.user = action.payload;
+        },
+        [register.rejected]:(state,action) => {
+            state.loading = false;
+            state.error = action.payload.message;
+        },
+
     },
 });
 
