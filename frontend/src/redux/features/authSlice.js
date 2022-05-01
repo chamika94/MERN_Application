e1fa0ed289fc4495b3ce7942951d63b38ee87003
chamika94 <chamikasandaruwan1994@gmail.com>
2022-05-01 17:22:39 +0530

    Completed loging part with all authentication and success messages and error messages

---
+++ frontend/src/redux/features/authSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk, applyMiddleware } from "@reduxjs/toolkit";
+import * as api from "../api";
+
+export const login = createAsyncThunk("auth/login",async({formValue, navigate, toast},{rejectWithValue})=>{
+    try{
+      const response = await api.signIn(formValue);
+      toast.success("Login Successfully");
+      navigate("/");
+      return response.data;
+     //console.log("resp",response);
+    }catch(err){
+        //console.log("resp",err.response.data.message);
+        return rejectWithValue(err.response.data);
+    }
+})
+
+const authSlice = createSlice({
+    name:"Auth",
+    initialState:{
+        user:null,
+        error:"",
+        loading: false,
+    },
+    extraReducers:{
+        [login.pending]:(state, action) => {
+            state.loading=true;
+        },
+        [login.fulfilled]:(state, action) => {
+            state.loading = false;
+            localStorage.setItem("profile",JSON.stringify({...action.payload}));
+            state.user = action.payload;
+        },
+        [login.rejected]:(state,action) => {
+            state.loading = false;
+            state.error = action.payload.message;
+        },
+    },
+});
+
+export default authSlice.reducer;
